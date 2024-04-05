13944a655deab0ef9383588237fade92ecc8fc57
Joey de Ruiter <contact@joeyderuiter.me>
2024-04-05 15:47:54 +0200

    Able to use serve multiple clients

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,45 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 use anyhow::{anyhow, Result};
+use tokio::{io::{AsyncWriteExt}, net::{TcpListener, TcpStream}};
 
+#[derive(Debug)]
 enum Command {
     Ping,
     Quit
 }
 
-fn read_command(mut stream: &mut TcpStream) -> Result<Command> {
-    let mut buffer = [0; 1024];
+fn read_command(stream: &TcpStream) -> Result<Command> {
+    let mut buffer = [0; 512];
+    let bytes_to_read = stream.try_read(&mut buffer)?;
 
-    let bytes_to_read = stream.read(&mut buffer)?;
     if bytes_to_read == 0 { return Ok(Command::Quit); }
 
     let command = std::str::from_utf8(&buffer[0..bytes_to_read])?;
 
     match command {
-        "*1\r\n$4\r\nping\r\n" => Ok(Command::Ping),
+        "*1\r\n$4\r\nPING\r\n" => Ok(Command::Ping),
         "quit\n" => Ok(Command::Quit),
         _ => Err(anyhow!("Unknown command"))
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-                loop {
-                    if let Ok(command) = read_command(&mut stream) {
-                        match command {
-                            Command::Ping => {
-                                stream.write(b"+PONG\r\n").unwrap();
-                            },
-                            Command::Quit => {
-                                break;
-                            }
-                        }    
-                    }
-                }
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let command = read_command(&socket).unwrap();
+
+            match command {
+                Command::Ping => {
+                    socket.write(b"+PONG\r\n").await.unwrap();
+                },
+                Command::Quit => { }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
