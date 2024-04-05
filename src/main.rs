4ce89c74479833a2202b58d4897e3c438ba2f41a
Joey de Ruiter <contact@joeyderuiter.me>
2024-04-05 17:58:28 +0200

    Added parsing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
 use anyhow::{anyhow, Result};
+use bytes::BytesMut;
+use messages::unpack_string;
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}};
+
+use crate::messages::Message;
+
+mod messages;
 
 #[derive(Debug)]
 enum Command {
+    Echo(Vec<Message>),
     Ping,
     Quit
 }
 
 async fn read_command(stream: &mut TcpStream) -> Result<Command> {
-    let mut buffer = [0; 512];
-    let bytes_to_read = stream.read(&mut buffer).await.unwrap();
+    let mut buffer = BytesMut::with_capacity(512);
+    let bytes_to_read = stream.read_buf(&mut buffer).await?;
 
     if bytes_to_read == 0 { return Ok(Command::Quit); }
+    let (command, args) = parse_command(&buffer)?;
+    let command = command.to_lowercase();
 
-    // let command = std::str::from_utf8(&buffer[0..bytes_to_read])?;
+    match command.as_str() {
+        "ping" => { Ok(Command::Ping) },
+        "echo" => { Ok(Command::Echo(args)) },
+        _ => Err(anyhow!("Unsupported command"))
+    }
 
-    Ok(Command::Ping)
+    // Ok(Command::Ping)
 }
+
+fn parse_command(buffer: &BytesMut) -> Result<(String, Vec<Message>)> {
+    let message = Message::parse(buffer)?;
+
+    match message {
+        Message::Array(x) => { 
+            let command = unpack_string(x.first().unwrap())?;
+            let args = x.into_iter().skip(1).collect();
+
+            Ok((command, args))
+        },
+        _ => Err(anyhow!("Unexpected command format"))
+    }
+} 
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -32,12 +59,23 @@ async fn main() -> Result<()> {
                 if let Ok(command) = read_command(&mut socket).await {
                     match command {
                         Command::Ping => {
-                            socket.write(b"+PONG\r\n").await.unwrap();
+                            if let Ok(serialized) = Message::SimpleString(String::from("PONG")).serialize() {
+                                socket.write(serialized.as_bytes()).await.unwrap();
+                            }
+                        },
+                        Command::Echo(values) => {
+                            if let Some(message) = values.first() {
+                                if let Ok(serialized) = message.clone().serialize() {
+                                    socket.write(serialized.as_bytes()).await.unwrap();
+                                }
+                            }
                         },
                         Command::Quit => {
                             break;
                         }
                     }
+                } else {
+                    break;
                 }
             }
         });
---
+++ src/messages.rs
@@ -0,0 +1,105 @@
+use std::{any, str, vec};
+use anyhow::{anyhow, Result};
+use bytes::BytesMut;
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    SimpleString(String),
+    BulkString(String),
+    Array(Vec<Message>)
+}
+
+impl Message {
+    pub fn serialize(self) -> Result<String> {
+        match self {
+            Message::SimpleString(s) => Ok(format!("+{}\r\n", s)),
+            Message::BulkString(s) => Ok(format!("${}\r\n{}\r\n", s.chars().count(), s)),
+            _ => { Err(anyhow!("impl the rest of the values"))}
+        }
+    }
+
+    pub fn parse(bytes: &BytesMut) -> Result<Self> {
+        let (message, _) = parse_message(bytes)?;
+
+        Ok(message)
+    }
+}
+
+fn parse_message(bytes: &BytesMut) -> Result<(Message, usize)> {
+   match bytes[0] as char {
+        '*' => parse_array(bytes),
+        '+' => parse_simple_string(bytes),
+        '$' => parse_bulk_string(bytes),
+        _ => { Err(anyhow!("Unsupported message type. {:?}", bytes))}
+    }
+}
+
+fn parse_array(bytes: &BytesMut) -> Result<(Message, usize)> {
+    let (array_items, mut bytes_consumed) = if let Some((line, len)) = read_until_crlf(&bytes[1..]) {
+        let array_items = parse_int(line).unwrap();
+
+        (array_items, len + 1)
+    } else {
+        return Err(anyhow!("Invalid array, {:?}", bytes));
+    };
+
+    let mut items = vec![];
+
+    for _ in 0..array_items {
+        let (array_item, len) = parse_message(&BytesMut::from(&bytes[bytes_consumed..]))?;
+
+        items.push(array_item);
+        bytes_consumed += len;
+    }
+
+    Ok((Message::Array(items), bytes_consumed))
+}
+
+fn parse_simple_string(bytes: &BytesMut) -> Result<(Message, usize)> {
+    if let Some((line, len)) = read_until_crlf(&bytes[1..]) {
+        let string = String::from_utf8(line.to_vec())?;
+
+        return Ok((Message::SimpleString(string), len + 1));
+    }
+
+    Err(anyhow!("Invalid simple string, {:?}", bytes))
+}
+
+fn parse_bulk_string(bytes: &BytesMut) -> Result<(Message, usize)> {
+    let (str_len, bytes_consumed) = if let Some((line, len)) = read_until_crlf(&bytes[1..]) {
+        let str_len = parse_int(line).unwrap();
+
+        (str_len, len + 1)
+    } else {
+        return Err(anyhow!("Invalid bulk string, {:?}", bytes));
+    };
+
+    let end_of_bulk_str = bytes_consumed + str_len as usize;
+    let total_length = end_of_bulk_str + 2;
+
+    let string = String::from_utf8(bytes[bytes_consumed..end_of_bulk_str].to_vec())?;
+
+    Ok((Message::BulkString(string), total_length))
+}
+
+fn parse_int(buffer: &[u8]) -> Result<i64> {
+    Ok(String::from_utf8(buffer.to_vec())?.parse::<i64>()?)
+}
+
+fn read_until_crlf(buffer: &[u8]) -> Option<(&[u8], usize)> {
+    for i in 1..buffer.len() {
+        if buffer[i - 1] == b'\r' && buffer[i] == b'\n' {
+            return Some((&buffer[0..(i - 1)], i + 1));
+        }
+    }
+
+    None
+}
+
+pub fn unpack_string(message: &Message) -> Result<String> {
+    match message {
+        Message::SimpleString(s) => Ok(s.clone()),
+        Message::BulkString(s) => Ok(s.clone()),
+        _ => Err(anyhow!("Unexpected value to unpack from message"))
+    }
+}
