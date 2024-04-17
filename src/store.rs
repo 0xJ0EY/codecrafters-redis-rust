45e75bf44a167dcbe29cf9372fd13d388ec12619
Joey de Ruiter <contact@joeyderuiter.me>
2024-04-17 10:48:37 +0200

    Clean up of code

--- src/store.rs
+++ src/store.rs
@@ -1,15 +1,22 @@
-use core::panic;
-use std::{collections::HashMap, env, path::Path, sync::Arc, time::{Duration, SystemTime, UNIX_EPOCH}};
+use std::{
+    collections::HashMap,
+    env,
+    path::Path,
+    sync::Arc,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
-use tokio::{fs::{metadata, File}, io::AsyncReadExt};
 use anyhow::{bail, Result};
+use tokio::{
+    fs::{metadata, File},
+    io::AsyncReadExt,
+};
 
 use crate::{configuration::ServerInformation, util::decode_hex};
 
 #[derive(Debug, Clone)]
 pub struct Entry {
     pub value: String,
-    pub expiry_time: Option<Duration>,
     pub expiry_at: Option<SystemTime>,
 }
 
@@ -21,11 +28,13 @@ impl Entry {
 
             Self {
                 value,
-                expiry_time: expiry,
                 expiry_at: Some(expiry_time),
-             }
+            }
         } else {
-            Self { value, expiry_time: None, expiry_at: None }
+            Self {
+                value,
+                expiry_at: None,
+            }
         }
     }
 }
@@ -37,7 +46,9 @@ pub struct Store {
 
 impl Store {
     pub fn new() -> Self {
-        Self { data: HashMap::new() }
+        Self {
+            data: HashMap::new(),
+        }
     }
 
     pub fn keys(&self) -> Vec<String> {
@@ -55,7 +66,9 @@ impl Store {
         let entry = entry.unwrap();
 
         if let Some(expiry_date_time) = entry.expiry_at {
-            if SystemTime::now() > expiry_date_time { return None }
+            if SystemTime::now() > expiry_date_time {
+                return None;
+            }
         }
 
         Some(entry)
@@ -74,7 +87,7 @@ pub fn full_resync_rdb() -> Vec<u8> {
     const EMPTY_RDB: &str = include_str!("empty_rdb.hex");
     let content = decode_hex(EMPTY_RDB).unwrap_or_default();
     let header = format!("${}\r\n", content.len()).as_bytes().to_vec();
-    
+
     [header, content].concat()
 }
 
@@ -84,22 +97,40 @@ pub async fn read_rdb_from_file(information: &Arc<ServerInformation>) -> Option<
     let directory = config.dir.clone();
     let filename = config.dbfilename.clone();
 
-    let file_name = if let Some(filename) = filename { filename } else { return None };
+    let file_name = if let Some(filename) = filename {
+        filename
+    } else {
+        return None;
+    };
 
-    let current_dir = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let current_dir = env::current_dir()
+        .unwrap()
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
     let directory = directory.unwrap_or(current_dir);
 
     let path = Path::new(&directory).join(Path::new(&file_name));
 
-    let mut f = if let Ok(file) = File::open(&path).await { file } else { return None; };
-    let metadata = if let Ok(metadata) = metadata(&path).await { metadata } else { return None; };
+    let mut f = if let Ok(file) = File::open(&path).await {
+        file
+    } else {
+        return None;
+    };
+
+    let metadata = if let Ok(metadata) = metadata(&path).await {
+        metadata
+    } else {
+        return None;
+    };
+
     let mut buffer = vec![0; metadata.len() as usize];
 
     _ = f.read(&mut buffer).await;
 
     Some(buffer)
 }
-
 
 fn parse_magic_number(data: &Vec<u8>, marker: &mut usize) -> bool {
     let magic_number = b"REDIS";
@@ -122,47 +153,11 @@ fn find_database_selector(data: &Vec<u8>, database: u8, marker: &mut usize) -> b
     false
 }
 
-fn read_length_encoded_int(data: &Vec<u8>, marker: &mut usize) -> Option<u64> {
-    let original = data[*marker];
-    let tag = original >> 6 & 0x03;
-
-    *marker += 1;
-
-    match tag {
-        0b11 => { todo!("String encoding"); }
-        0b10 => { 
-            // TODO: Implement this
-            let octet1 = data[*marker]; *marker += 1;
-            let octet2 = data[*marker]; *marker += 1;
-
-            let length = u16::from_le_bytes([octet1, octet2]) as u64;
-
-            if length == 0 {
-                Some(0)
-            } else {
-                todo!("implement more parsing")
-            }
-         }
-        0b01 => { 
-            let octet1 = original & 0xFC;
-            let octet2 = data[*marker];
-            *marker += 1;
-
-            Some(u16::from_le_bytes([octet1, octet2]) as u64)
-         }
-        0b00 => { 
-            let length = u8::from_le(original & 0x3F);
-
-            if length == 0 { return Some(0); }
-
-            todo!("Implement the rest of the next 6 bits represent the length");
-        },
-        _ => { panic!("Unreachable statement"); }
-    }
-}
-
 fn read_resizedb_field(data: &Vec<u8>, marker: &mut usize) -> bool {
-    if data[*marker] != 0xFB { return false; }
+    if data[*marker] != 0xFB {
+        return false;
+    }
+
     *marker += 1;
 
     // TODO: I don't know how to really parse the length of the hash table, so we hardcode it for now
@@ -173,26 +168,30 @@ fn read_resizedb_field(data: &Vec<u8>, marker: &mut usize) -> bool {
 }
 
 fn read_length_prefixed_string(data: &Vec<u8>, marker: &mut usize) -> Option<String> {
-    let length = data[*marker] as usize; *marker += 1;
+    let length = data[*marker] as usize;
+    *marker += 1;
 
     let start = *marker;
     let end = start + length;
 
     let slice: &[u8] = &data[start..end];
     let value = std::str::from_utf8(slice).unwrap().to_string();
-    
+
     *marker = end;
 
     Some(value)
 }
 
-
 fn read_normal_string_value(data: &Vec<u8>, marker: &mut usize) -> Result<(String, String)> {
-    let key = if let Some(key) = read_length_prefixed_string(data, marker) { key } else {
+    let key = if let Some(key) = read_length_prefixed_string(data, marker) {
+        key
+    } else {
         bail!("Unable to read key from the entry");
     };
 
-    let value = if let Some(value) = read_length_prefixed_string(data, marker) { value } else {
+    let value = if let Some(value) = read_length_prefixed_string(data, marker) {
+        value
+    } else {
         bail!("Unable to read value from the entry");
     };
 
@@ -219,23 +218,32 @@ fn read_entry(data: &Vec<u8>, marker: &mut usize) -> Result<(String, Entry)> {
 
             offset += 8;
 
-            let value_type = data[offset]; offset += 1;
-            if value_type != 0x00 { bail!("Unsupported value"); }
+            let value_type = data[offset];
+            offset += 1;
+            if value_type != 0x00 {
+                bail!("Unsupported value");
+            }
 
             let (key, value) = read_normal_string_value(data, &mut offset)?;
 
             let expiry = UNIX_EPOCH + Duration::from_millis(expiry_time);
             let current = SystemTime::now();
 
-            let duration = expiry.duration_since(current).unwrap_or_else(|_| Duration::from_secs(0));
+            let duration = expiry
+                .duration_since(current)
+                .unwrap_or_else(|_| Duration::from_secs(0));
 
             *marker = offset;
 
             Ok((key, Entry::new(value, Some(duration))))
         }
         _ => {
-            let value_type = data[offset]; offset += 1;
-            if value_type != 0x00 { bail!("Unsupported value"); }
+            let value_type = data[offset];
+            offset += 1;
+
+            if value_type != 0x00 {
+                bail!("Unsupported value");
+            }
 
             let (key, value) = read_normal_string_value(data, &mut offset)?;
 
@@ -249,9 +257,15 @@ fn read_entry(data: &Vec<u8>, marker: &mut usize) -> Result<(String, Entry)> {
 fn parse_rdb(store: &mut Store, data: &Vec<u8>) {
     let mut marker = 0;
 
-    if !parse_magic_number(data, &mut marker) { return; }
-    if !find_database_selector(data, 0x00, &mut marker) { return; }
-    if !read_resizedb_field(data, &mut marker) { return; }
+    if !parse_magic_number(data, &mut marker) {
+        return;
+    }
+    if !find_database_selector(data, 0x00, &mut marker) {
+        return;
+    }
+    if !read_resizedb_field(data, &mut marker) {
+        return;
+    }
 
     while data[marker] != 0xFF {
         let result = read_entry(data, &mut marker);
